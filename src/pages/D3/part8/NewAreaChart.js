4da59d7e899cd603bf340beed25799e448a1ee87
MingYu Zhu(朱明宇) <mingyuzhu@Hillstonenet.com>
2019-09-17 10:40:10 +0800

    面积图

--- src/pages/D3/part8/NewAreaChart.js
+++ src/pages/D3/part8/NewAreaChart.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from "react";
 
 import areaChart from "./utils/areaChart";
+import { Button } from "antd";
 
 const NewAreaChart = () => {
+  const chart = areaChart();
   useEffect(() => {
-    const chart = areaChart();
     chart.renderTo("#new-area-chart");
   }, []);
   return (
     <>
       <div id="new-area-chart"></div>
+      <Button
+        type="primary"
+        onClick={() => {
+          chart.update();
+        }}
+      >
+        更新
+      </Button>
     </>
   );
 };
--- src/pages/D3/part8/utils/areaChart.js
+++ src/pages/D3/part8/utils/areaChart.js
@@ -6,6 +6,9 @@ const areaChart = () => {
     _height = 400,
     _width = 800,
     margin = 25,
+    _bodyG,
+    _data,
+    _selector,
     _xScale = d3
       .scaleLinear()
       .domain([0, 10])
@@ -13,39 +16,66 @@ const areaChart = () => {
     _yScale = d3
       .scaleLinear()
       .domain([0, 10])
-      .range([_height - margin, margin]);
+      .range([_height - margin, margin]),
+    _color = d3.scaleOrdinal(d3.schemeCategory10),
+    _line = d3
+      .line()
+      .x(d => _xScale(d.x))
+      .y(d => _yScale(d.y)),
+    _area = d3
+      .area()
+      .x(d => _xScale(d.x))
+      .y0(_yScale(0))
+      .y1(d => _yScale(d.y));
 
   _chart.renderTo = selector => {
-    _svg = d3
-      .select(selector)
-      .append("svg")
-      .attr("height", _height)
-      .attr("width", _width);
+    if (selector) {
+      _selector = selector;
+    }
+    if (!_svg) {
+      _svg = d3
+        .select(_selector)
+        .append("svg")
+        .attr("height", _height)
+        .attr("width", _width);
+      _chart
+        .renderAxes()
+        .renderClipBody()
+        .renderBody();
+    }
 
-    _chart.renderAxes().renderClipBody();
+    _chart
+      .data(randomData())
+
+      .renderDots()
+      .renderLine()
+      .renderArea();
+
     return _chart;
   };
 
   _chart.renderClipBody = () => {
-    // svg
-    // .append("defs")
-    // .append("clipPath")
-    // .attr("id", "body-clip")
-    // .append("rect")
-    // .attr("x", 0 - padding)
-    // .attr("y", 0)
-    // .attr("width", quadrantWidth() + 2 * padding)
-    // .attr("height", quadrantHeight());
     _svg
       .append("defs")
       .append("clipPath")
       .attr("id", "body-clip")
       .append("rect")
-      .attr("x", margin)
-      .attr("y", margin)
-      .attr("width", _width - 2 * margin)
-      .attr("height", _height - 2 * margin);
+      .attr("x", margin - 5)
+      .attr("y", margin - 5)
+      .attr("width", _width - 2 * margin + 10)
+      .attr("height", _height - 2 * margin + 10);
 
+    return _chart;
+  };
+
+  _chart.renderBody = () => {
+    if (!_bodyG) {
+      _bodyG = _svg
+        .append("g")
+        .attr("class", "body")
+        .attr("transform", "translate(0,0)")
+        .attr("clip-path", "url(#body-clip)");
+    }
     return _chart;
   };
 
@@ -84,9 +114,78 @@ const areaChart = () => {
     return _chart;
   };
 
-  _chart.renderDots = () => {};
+  _chart.data = data => {
+    if (data !== undefined) {
+      _data = data;
+      return _chart;
+    }
+    return _data;
+  };
 
+  _chart.renderDots = () => {
+    if (_data && _data.length > 0) {
+      _data.forEach((item, i) => {
+        const dots = _bodyG.selectAll("circle.dot_" + i).data(item);
+        dots
+          .enter()
+          .append("circle")
+          .merge(dots)
+          .attr("class", "dot_" + i)
+          .style("stroke", _color(i))
+          .transition()
+          .attr("cx", d => _xScale(d.x))
+          .attr("cy", d => _yScale(d.y))
+          .attr("r", 4);
+      });
+    }
+    return _chart;
+  };
+
+  _chart.renderLine = () => {
+    const linePath = _bodyG.selectAll("path.line").data(_data);
+    linePath
+      .enter()
+      .append("path")
+      .merge(linePath)
+      .attr("class", "line")
+      .style("stroke", (d, i) => _color(i))
+      .transition()
+      .attr("d", d => _line(d));
+    return _chart;
+  };
+
+  _chart.renderArea = () => {
+    const areaPath = _bodyG.selectAll("path.area").data(_data);
+
+    areaPath
+      .enter()
+      .append("path")
+      .merge(areaPath)
+      .attr("class", "area")
+      .style("fill", (d, i) => _color(i))
+      .transition()
+      .attr("d", d => _area(d));
+    return _chart;
+  };
+
+  _chart.update = data => {
+    if (!data) {
+      data = randomData();
+    }
+
+    _chart.renderTo();
+    return _chart;
+  };
   return _chart;
+};
+
+const randomData = () => {
+  return d3.range(2).map(i => {
+    return d3.range(11).map(i => ({
+      x: i,
+      y: Math.random() * 10
+    }));
+  });
 };
 
 export default areaChart;
